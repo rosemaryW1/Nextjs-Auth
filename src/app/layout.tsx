b5e7b682eebf2a6b6fc7263483dda6158820eba5
rosemaryW1 <rosemary.wakaba@strathmore.edu>
2023-12-12 10:17:19 +0300

    Project Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="min-h-screen flex-col items-center justify-between p-24 ">{children}</body>
     </html>
   )
 }
