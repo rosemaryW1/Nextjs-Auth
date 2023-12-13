65fc41ec76763010fceb582720472bd893b9171c
rosemaryW1 <rosemary.wakaba@strathmore.edu>
2023-12-13 22:43:04 +0300

    clerk auth

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 export default function Dashboardpage() {
-  return <div> Dashboard Page</div>;
+  return <div className="flex items-center justify-center min-h-screen">
+     Dashboard Page
+     </div>;
  
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Nav from './components/Nav'
+import { ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className="min-h-screen ">
         <Nav />
@@ -25,5 +27,6 @@ export default function RootLayout({
         
         </body>
     </html>
+    </ClerkProvider>
   )
 }
